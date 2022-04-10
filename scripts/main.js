9b45ca485b71bad75de0e599d69a0a4fcd93639d
mohammad_lbf <labbaf2016@gmail.com>
2022-04-10 17:34:28 +0430

    last section , map with leaflet library & form added & project finished

--- scripts/main.js
+++ scripts/main.js
@@ -57,4 +57,13 @@ setInterval(function(){
     }
    
 },1000)
-// *******************************************************************************************************************
+// **********************************************---map---*********************************************************************
+let map = L.map('map').setView([35.673641, 51.489171], 17);
+L.tileLayer('https://api.mapbox.com/styles/v1/{id}/tiles/{z}/{x}/{y}?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    maxZoom: 18,
+    id: 'mapbox/streets-v11',
+    tileSize: 512,
+    zoomOffset: -1,
+    accessToken: 'your.mapbox.access.token'
+}).addTo(map);
